46650b6ce70cf8cb167806ecabddb08455003511
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-10-21 03:13:11 +0100

    Company module

---
+++ Modules/Company/database/seeders/CompanyDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Duobix\Company\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CompanyDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
