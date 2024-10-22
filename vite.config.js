8723a0e1dd97a7a6406fbefd28cf98867ff8f5bc
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-10-22 19:37:17 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import collectModuleAssetsPaths from "./vite-module-loader";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default async function getConfig() {
+    const paths = [
+        'resources/css/app.css',
+        'resources/js/app.js',
+    ];
+
+    const allPaths = await collectModuleAssetsPaths(paths, 'Modules');
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: allPaths,
+                refresh: true,
+            })
+        ]
+    });
+}
+
