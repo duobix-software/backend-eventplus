645531683cc004041a76dcf62f98a8fdc47434b5
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-10-23 14:55:32 +0100

    company web routes

--- Modules/Company/routes/web.php
+++ Modules/Company/routes/web.php
@@ -6,6 +6,6 @@ use Duobix\Company\Http\Controllers\EventController;
 Route::prefix('company')->group(function () {
     require __DIR__ . '/auth_routes.php';
 
-    Route::resource('events', [EventController::class])->names('events');
+    Route::resource('events', EventController::class)->names('events');
 
 });
