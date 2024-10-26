5ad31b08b17a3347efc14d462a1ef193ac2963b3
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-10-26 03:06:18 +0100

    moving css,js to the top level directory resources.

--- Modules/Admin/resources/assets/js/pages/welcome.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const welcome = () => {
-  return (
-    <div>welcom 1e</div>
-  )
-}
-
-export default welcome
---
+++ Modules/Admin/resources/js/pages/welcome.tsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/react";
+
+const welcome = () => {
+    return (
+        <div>
+            <Head title="Dashboard" />
+            Hi,
+        </div>
+    );
+};
+
+export default welcome;
