b5ccfb6b095e9e33eaf42e7d140e4194627804f1
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-11-01 18:53:50 +0100

    add method findByFieldOrFail to the base repository

--- Modules/Core/src/Eloquent/Repository.php
+++ Modules/Core/src/Eloquent/Repository.php
@@ -9,4 +9,13 @@ use Prettus\Repository\Traits\CacheableRepository;
 abstract class Repository extends BaseRepository implements CacheableInterface
 {
     use CacheableRepository;
+
+    public function findByFieldOrFail($field, $value = null, $columns = ['*'])
+    {
+        $value = $this->findByField($field, $value, $columns);        
+
+        if (!count($value)) return abort(404);
+
+        return $value;
+    }
 }
