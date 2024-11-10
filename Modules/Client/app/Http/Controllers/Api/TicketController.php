8f91f9aabb36ef577485b84f88d85379195146b3
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-11-10 01:25:51 +0100

    ticketcontroller updated

--- Modules/Client/app/Http/Controllers/Api/TicketController.php
+++ Modules/Client/app/Http/Controllers/Api/TicketController.php
@@ -4,7 +4,6 @@ namespace Duobix\Client\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Duobix\Event\Repositories\EventRepository;
 use Duobix\Sales\Repositories\OrderRepository;
 use Duobix\Ticket\Repositories\TicketRepository;
 
@@ -13,7 +12,6 @@ class TicketController extends Controller
 
     public function __construct(
         protected OrderRepository $orderRepository,
-        protected EventRepository $eventRepository,
         protected TicketRepository $ticketRepository
     ) {}
 
@@ -23,15 +21,15 @@ class TicketController extends Controller
     public function __invoke(Request $request)
     {
         /** @var \Duobix\Sales\Models\Order */
-        $order = $this->orderRepository->findByFieldOrFail('id', $request->route('order'))->first();        
+        $order = $this->orderRepository->findByFieldOrFail('id', $request->route('order'))->first();
 
         if (!$order->canIssueTicket()) return abort(403);
 
         $accessTicket = $this->ticketRepository->createOrRefrsh($order);
 
         return [
-            'token' => $accessTicket->publicToken,
-            'fallback_token' => 'xyz'
+            'id' => $accessTicket->ticket->id,
+            'token' => url()->query('https://eventplus.duobix.com', ['token' => $accessTicket->publicToken]),
         ];
     }
 }
